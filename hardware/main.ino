3b35cc5ee55ecf219cd95586cc36f84d7ddaa505
qcoral <41168529+qcoral@users.noreply.github.com>
2024-06-30 17:31:28 -0400

    filled out interface function, working on wifi logic

--- hardware/main.ino
+++ hardware/main.ino
@@ -1,32 +1,88 @@
 #include <ArduinoJSON.h>
 #include <WiFi.h>
-#include <
+#include <Wire.h>
+// didn't add SPI library, there might be an issue!
+
 #include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+#define OLED_RESET -1       // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3D ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 /*
 LCD pin definitions here
 */
 
+// Button definitions
 int prev = 2;
 int next = 3;
+int index = 0;
 
-void setup () {
-    pinMode()
+String api_url = "https://hcattendance.com/api";
 
-}
+WiFiClient client;
 
-void displayData() {
-
-}
-
-void parseJsonData(){
+void displayData(String message)
+{
+    display.clear();
+    //the rest of the code here displays the message
     
 }
 
-void interface() {
+void parseJsonData(int index)
+{
+    HTTPClient http;
+    WiFiClient client;
 
+    // Send request
+    http.useHTTP10(true);
+    http.begin(client, api_url);
+    http.GET();
+
+    // Parse response
+    DynamicJsonDocument doc(2048);
+    deserializeJson(doc, http.getStream());
+
+    String messagefrag = doc[index]
+
+    // Disconnect
+    http.end();
+
+    displayData(index)
 }
 
-void loop() {
+void interface()
+{
+    if (prev)
+    {
+        index = 0 ? index = 6 : index--;
+        delay(300);
+    }
+    else if (next)
+    {
+        index == 6 ? index = 0 : index++;
+        delay(300)
+    }
+    else
+    {
+        return;
+    }
 
+    parseJsonData(index);
+}
+
+void setup()
+{
+    pinMode(prev, INPUT);
+    pinMode(next, INPUT);
+    parseJsonData(index);
+}
+
+void loop()
+{
+    interface();
 }

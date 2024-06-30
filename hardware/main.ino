7b03c27c324684b3654ab2468e09565bd32716d3
qcoral <41168529+qcoral@users.noreply.github.com>
2024-06-30 16:47:39 -0400

    initialized project, began importing libraries

---
+++ hardware/main.ino
@@ -0,0 +1,32 @@
+#include <ArduinoJSON.h>
+#include <WiFi.h>
+#include <
+#include <Adafruit_GFX.h>
+
+/*
+LCD pin definitions here
+*/
+
+int prev = 2;
+int next = 3;
+
+void setup () {
+    pinMode()
+
+}
+
+void displayData() {
+
+}
+
+void parseJsonData(){
+    
+}
+
+void interface() {
+
+}
+
+void loop() {
+
+}
